6cef70ae86c8639d5ebd2eaf6c167d4cfaacf858
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-02-11 00:39:24 -0300

    Redux Toolkit

--- redux-basico/react-app/src/index.js
+++ redux-basico/react-app/src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-
-function reducer(state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENTAR':
-      return state + 1;  
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+import { Provider } from 'react-redux';
+import store from './store/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,5 +10,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
