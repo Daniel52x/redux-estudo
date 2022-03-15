2c9f25765072db7d4a8ffebd9d74a6880c3944b4
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-03-14 21:52:30 -0300

    seletor

--- redux-basico/modulo5/cache/src/store/photos.js
+++ redux-basico/modulo5/cache/src/store/photos.js
@@ -3,13 +3,25 @@ import createAsyncSlice from './helper/createAsyncSlice';
 const slice = createAsyncSlice({
   name: 'photos',
   fetchConfig: () => ({
-    url: 'https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0',
+    url: 'https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=10&_user=0',
     options: {
       method: 'GET',
       cache: 'no-store',
     },
   }),
 });
+
+// const getOverFiveKgre = createSelector((state) => state.photos.data, (data))
+
+export const getOverFiveKg = (state) => {
+  const { data } = state.photos;
+  const overFiveKg = data?.filter(({ peso }) => peso > 5);
+  const transformPuond = overFiveKg?.map((photo) => ({
+    ...photo,
+    peso: Math.floor(photo.peso * 2.2),
+  }));
+  return transformPuond;
+};
 
 export const fetchPhotos = slice.asyncAction;
 
